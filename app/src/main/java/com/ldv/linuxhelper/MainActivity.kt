7d0718b4bbd3a3a9b52ef761774c03b0fe3d3365
LDmitriyV <71625536+LDmitriyV@users.noreply.github.com>
2023-04-11 22:41:51 +0600

    intial

--- app/src/main/java/com/ldv/linuxhelper/MainActivity.kt
+++ app/src/main/java/com/ldv/linuxhelper/MainActivity.kt
@@ -3,9 +3,8 @@ package com.ldv.linuxhelper
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.elevation.SurfaceColors
@@ -25,8 +24,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+//        val navController = Navigation.findNavController(this,R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -34,7 +33,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_favourites, R.id.navigation_tips, R.id.settings_button, R.id.search_button
             )
         )
-        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navHostFragment.navController)
         val color = SurfaceColors.SURFACE_2.getColor(this)
         window.statusBarColor = color
         window.navigationBarColor = color
